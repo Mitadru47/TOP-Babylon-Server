ee3a7ea341662fab59823aa22bd2e6299a4f359b
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-03-25 21:40:15 +0530

    Post Model Updated - New Virtual Property 'momentDateEdited'

--- models/post.js
+++ models/post.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const moment = require("moment");
+
 const PostSchema = new Schema({
 
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
@@ -15,5 +17,12 @@ const PostSchema = new Schema({
 PostSchema.virtual("url").get(function (){
     return "/posts/" + this._id;
 });
+
+PostSchema.virtual("momentDateEdited").get(function (){ 
+    return moment(this.dateEdited).format('LLL'); 
+});
+
+PostSchema.set('toObject', { virtuals: true });
+PostSchema.set('toJSON', { virtuals: true });
 
 module.exports = mongoose.model("Post", PostSchema);
